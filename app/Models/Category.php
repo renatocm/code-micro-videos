02ac86832fd952ec3194086fcf86fc990d4c85c9
Renato Alencar <renatoalencar@MacBook-Pro-de-Renato.local>
2021-11-26 01:51:55 -0300

    finish category

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,9 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Category extends Model
 {
+    use SoftDeletes, \App\Models\Traits\Uuid;
+
     protected $fillable = ['name', 'description', 'is_active'];
+    protected $dates = ['deleted_at'];
+    protected $casts = [
+        'id' => 'string'
+    ];
 
 }
